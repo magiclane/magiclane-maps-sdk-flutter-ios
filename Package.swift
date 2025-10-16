805f889631db05699a2a455ec351866d31b6f2cc
MagicLane Developer <magiclane-developer@users.noreply.github.com>
2025-10-16 13:07:42 +0300

    Update SPM configs for 3.1.0 release

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
      targets: [
         .binaryTarget(
             name: "GEMKit",
-            url: "https://developer.magiclane.com/packages/ios/flutter/3.0.3/GEMKit.xcframework.zip",
-            checksum: "51e8326c71c778a4b400c34ed18c0851edd8b30b92f2e5c8b7140938b954eb5e"
+            url: "https://developer.magiclane.com/packages/ios/flutter/3.1.0/GEMKit.xcframework.zip",
+            checksum: "692ba9d7fddaf0593576471b943a7cac7b4bfa41aa205e7d0399b2b80345225f"
         )
     ]
 )
